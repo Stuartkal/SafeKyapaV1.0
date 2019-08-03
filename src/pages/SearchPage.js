2516acd3b2a9b0d322b5cd45ea2024aa0fa50a57
Stuartkal <Stuartkal@gmail.com>
2019-08-03 19:56:09 +0300

    search map

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Paper,IconButton,InputBase, Divider} from '@material-ui/core'
+
+import SearchIcon from '@material-ui/icons/Search';
+
 
 function SearchPage() {
     return (
         <div>
-            <p>SearchPage</p>
+    <Paper >
+        <IconButton  aria-label="search">
+        <SearchIcon />
+        </IconButton>
+      <InputBase
+        placeholder="Search Google Maps"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+      <Divider  />
+      
+      
+    </Paper>
         </div>
     )
 }
