70e94f10abb5e2aea99f0f8a21641df550ddbdc3
albystein <atemsunday@gmail.com>
2019-08-04 09:19:46 +0300

    Deploy ready

---
+++ build/precache-manifest.ead7824e9c9bd32151d5bba774e91cc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42890dc54045e613e5c3b54e114619d9",
+    "url": "/safekyapa/index.html"
+  },
+  {
+    "revision": "8631acf8766cfe869a35",
+    "url": "/safekyapa/static/css/main.27d5520f.chunk.css"
+  },
+  {
+    "revision": "f69d8ea931d9b650d5c6",
+    "url": "/safekyapa/static/js/2.f2c3af0c.chunk.js"
+  },
+  {
+    "revision": "8631acf8766cfe869a35",
+    "url": "/safekyapa/static/js/main.f6938c6a.chunk.js"
+  },
+  {
+    "revision": "5d4206cf39ffa7f8102e",
+    "url": "/safekyapa/static/js/runtime~main.c3795255.js"
+  },
+  {
+    "revision": "393de9fbb5bd7c24c5b3a8c916bb8596",
+    "url": "/safekyapa/static/media/land.393de9fb.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18350b340084cfd19a0ae340b2325aa3.js"
+  "/safekyapa/precache-manifest.ead7824e9c9bd32151d5bba774e91cc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/safekyapa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
