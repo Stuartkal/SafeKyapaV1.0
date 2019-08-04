5cf2365e2eca083014df743b63fa9b0211ff43b6
albystein <atemsunday@gmail.com>
2019-08-04 08:56:27 +0300

    Build generated

---
+++ build/precache-manifest.18350b340084cfd19a0ae340b2325aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25d243ed18931b599d7a7db6061fc68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21ba1593079535d459ea",
+    "url": "/static/css/main.27d5520f.chunk.css"
+  },
+  {
+    "revision": "b2a2b43de7c9a6887695",
+    "url": "/static/js/2.c0ad4cf9.chunk.js"
+  },
+  {
+    "revision": "21ba1593079535d459ea",
+    "url": "/static/js/main.40a8491c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "393de9fbb5bd7c24c5b3a8c916bb8596",
+    "url": "/static/media/land.393de9fb.png"
+  }
+]);
